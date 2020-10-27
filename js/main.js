497a242cd8e09607a7b25e9250958b38e7c894d4
Maiyo P <maiyopabz@gmail.com>
2020-10-26 20:54:46 -0700

    HomeContainer, img, styles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) => {
+const showMenu = (toggleId, navId)=> {
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
 
@@ -38,7 +38,7 @@ function scrollActive() {
         const sectionTop = current.offsetTop - 50
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionsTop + sectionHeight){
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.add('active')
         }else {
             document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.remove('active')
@@ -48,3 +48,8 @@ function scrollActive() {
 }
 
 /*===== CHANGE COLOR HEADER =====*/ 
+
+window.onscroll = () => {
+    const nav = document.getElementById('header')
+    if (this.scrollY >=200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
